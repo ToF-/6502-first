ead20a1f050370fe46189fd6e6820fb92adf4c64
Christophe Thibaut <cthibauttof@gmail.com>
2024-11-22 15:17:49 +0100

    final

--- first.s
+++ first.s
@@ -1,12 +1,34 @@
-      lda #$00
+            lda #$00
       sta $42
-loop: lda $fe       ; $fe = random generator port
-      tay           ; transfer A to Y
+      lda #$01
+      sta $23
+
+
+loop:
       lda $fe
       and #$03
       clc
       adc #$02
       sta $43
+      ldy $fe
       lda $fe
+      and $23
       sta ($42),y
+      lda $ff
+      cmp #46
+      bne loop
+      inc $23
+      jsr init
       jmp loop
+
+init:
+      lda #$00
+      ldy #$00
+loop_init:
+      sta $200,y
+      sta $300,y
+      sta $400,y
+      sta $500,y
+      iny
+      bne loop_init
+      rts
